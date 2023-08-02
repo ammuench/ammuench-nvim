7cafd04e70558804698009f2fe5b7779c24bea7b
Alex Muench <ammuench@gmail.com>
2023-08-02 18:52:13 -0500

    🔧 Update default LSP and tools

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,7 +1,37 @@
 return {
   lsp = {
+    servers = {},
     formatting = {
       format_on_save = true, -- enable or disable automatic formatting on save
+    },
+  },
+  plugins = {
+    {
+      "mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = {
+          "angularls",
+          "cssls",
+          "html",
+          "lua_ls",
+          "tailwindcss",
+          "tsserver",
+          "volar",
+        },
+      },
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      lazy = false,
+      config = function()
+        require("mason-tool-installer").setup {
+          ensure_installed = {
+            "stylua",
+            "eslint_d",
+            "prettierd",
+          },
+        }
+      end,
     },
   },
 }
