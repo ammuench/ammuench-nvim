dcb8563122e52394a5eba76f84b64aa75aa99689
Alex Muench <ammuench@gmail.com>
2024-03-24 10:48:30 -0500

    ⏪ Revert vue changes

--- init.lua
+++ init.lua
@@ -1,23 +1,20 @@
-if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then
+    vim.loader.enable()
+end
 
-for _, source in ipairs {
-  "astronvim.bootstrap",
-  "astronvim.options",
-  "astronvim.lazy",
-  "astronvim.autocmds",
-  "astronvim.mappings",
-} do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+for _, source in ipairs {"astronvim.bootstrap", "astronvim.options", "astronvim.lazy", "astronvim.autocmds",
+                         "astronvim.mappings"} do
+    local status_ok, fault = pcall(require, source)
+    if not status_ok then
+        vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+    end
 end
 
 if astronvim.default_colorscheme then
-  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
-    require("astronvim.utils").notify(
-      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
-      vim.log.levels.ERROR
-    )
-  end
+    if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+        require("astronvim.utils").notify(("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+            vim.log.levels.ERROR)
+    end
 end
 
 require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
@@ -25,27 +22,26 @@ require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, f
 require "user"
 
 -- NVIM + VUE SETUP
-local mason_registry = require "mason-registry"
-local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-  .. "/node_modules/@vue/language-server"
-
-local lspconfig = require "lspconfig"
-
-lspconfig.tsserver.setup {
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = vue_language_server_path,
-        languages = { "vue" },
-      },
-    },
-  },
-  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-}
-
--- No need to set `hybridMode` to `true` as it's the default value
-lspconfig.volar.setup {}
+-- local mason_registry = require('mason-registry')
+-- local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+--
+-- local lspconfig = require('lspconfig')
+--
+-- lspconfig.tsserver.setup {
+--   init_options = {
+--     plugins = {
+--       {
+--         name = '@vue/typescript-plugin',
+--         location = vue_language_server_path,
+--         languages = { 'vue' },
+--       },
+--     },
+--   },
+--   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+-- }
+--
+-- -- No need to set `hybridMode` to `true` as it's the default value
+-- lspconfig.volar.setup {}
 -- END NVIM + VUE SETUP
 -- Set theme
 vim.cmd [[colorscheme nightfly]]
