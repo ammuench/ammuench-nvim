53c270d47cecb74bb392a40146e2ca76830d9767
Alex Muench <ammuench@gmail.com>
2024-04-12 21:00:09 -0500

    💄 Update indent colors and styles

--- lua/plugins/indent-colorizer.lua
+++ lua/plugins/indent-colorizer.lua
@@ -10,20 +10,6 @@ return {
     main = "ibl",
     opts = {},
     config = function()
-      -- -- uncomment for column zebra-striping instead
-      -- local highlight = {
-      --   "CursorColumn",
-      --   "Whitespace",
-      -- }
-      -- require("ibl").setup {
-      --   indent = { highlight = highlight, char = "" },
-      --   whitespace = {
-      --     highlight = highlight,
-      --     remove_blankline_trail = false,
-      --   },
-      --   scope = { enabled = false },
-      -- }
-      -- -- require("ibl").setup()
       local highlight = {
         "RainbowRed",
         "RainbowYellow",
@@ -34,15 +20,16 @@ return {
         "RainbowCyan",
       }
 
-      -- local highlightIndent = {
-      --   "RainbowRedIndent",
-      --   "RainbowYellowIndent",
-      --   "RainbowBlueIndent",
-      --   "RainbowOrangeIndent",
-      --   "RainbowGreenIndent",
-      --   "RainbowVioletIndent",
-      --   "RainbowCyanIndent",
-      -- }
+      local highlightIndent = {
+        "RainbowRedIndent",
+        "RainbowYellowIndent",
+        "RainbowBlueIndent",
+        "RainbowOrangeIndent",
+        "RainbowGreenIndent",
+        "RainbowVioletIndent",
+        "RainbowCyanIndent",
+      }
+
       local hooks = require "ibl.hooks"
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
@@ -54,19 +41,19 @@ return {
         vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
         vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-        -- vim.api.nvim_set_hl(0, "RainbowRedIndent", { fg = "#E06C75" })
-        -- vim.api.nvim_set_hl(0, "RainbowYellowIndent", { fg = "#E5C07B" })
-        -- vim.api.nvim_set_hl(0, "RainbowBlueIndent", { fg = "#61AFEF" })
-        -- vim.api.nvim_set_hl(0, "RainbowOrangeIndent", { fg = "#D19A66" })
-        -- vim.api.nvim_set_hl(0, "RainbowGreenIndent", { fg = "#98C379" })
-        -- vim.api.nvim_set_hl(0, "RainbowVioletIndent", { fg = "#C678DD" })
-        -- vim.api.nvim_set_hl(0, "RainbowCyanIndent", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowRedIndent", { fg = "#6e171e" })
+        vim.api.nvim_set_hl(0, "RainbowYellowIndent", { fg = "#765517" })
+        vim.api.nvim_set_hl(0, "RainbowBlueIndent", { fg = "#0c497a" })
+        vim.api.nvim_set_hl(0, "RainbowOrangeIndent", { fg = "#603d1d" })
+        vim.api.nvim_set_hl(0, "RainbowGreenIndent", { fg = "#3b5727" })
+        vim.api.nvim_set_hl(0, "RainbowVioletIndent", { fg = "#5a1b6d" })
+        vim.api.nvim_set_hl(0, "RainbowCyanIndent", { fg = "#1e4c52" })
       end)
 
       vim.g.rainbow_delimiters = { highlight = highlight }
       require("ibl").setup {
         scope = { highlight = highlight, char = "┃" },
-        indent = { highlight = highlight, char = "┆" },
+        indent = { highlight = highlightIndent, char = "╎" },
       }
 
       hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
