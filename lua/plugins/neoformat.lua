41b02074263b4cf6383ae09dd26876e320736ec4
Alex Muench <ammuench@gmail.com>
2024-07-04 12:01:09 -0500

    🚩 Disable auto-formatting

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -6,6 +6,6 @@ return {
       let g:neoformat_try_node_exe = 1
       let g:neoformat_only_msg_on_error = 1
     ]]
-    vim.cmd "autocmd BufWritePre *.vue,*.ts,*.js,*.jsx,*.tsx,*.html,*.md,*.css,*.scss,*.json Neoformat prettier"
+    -- vim.cmd "autocmd BufWritePre *.vue,*.ts,*.js,*.jsx,*.tsx,*.html,*.md,*.css,*.scss,*.json Neoformat prettier"
   end,
 }
